900fe2c587f408409683a2bb981697bf2bdaac08
joshuakibett <JOshuaulempoa772@gmail.com>
2023-04-26 16:21:37 +0300

    initial commit

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,21 @@
+fun main(args : Array<String>) {
+    //mutable variables
+    var name="Emobilis Mobile Technology"
+    var jina="Joshua"
+    var nambari=77
+    var num1=9
+    var num2=17
+    name="University of Nairobi"
+
+    println(name)
+    println(nambari)
+    println("My name is $jina")
+    println("My name is "+jina)
+    println("The product is "+num1 * num2)
+
+    //immutable variable
+    val mimi="Mr Kibet"
+//    val="Mr Joshua"
+    println("My name is $mimi")
+
+}
